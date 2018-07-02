3bea36c51d42d807dab2b492f64e5317eba11bf6
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2018-07-01 18:39:22 -0700

    Added test for comparing against instances of itself

--- tests/ComparableTest.php
+++ tests/ComparableTest.php
@@ -29,6 +29,18 @@ class ComparableTest extends TestCase
         $this->assertFalse($differs);
     }
 
+    public function test_it_can_determine_if_it_equals_another_instance_of_itself()
+    {
+        $string1 = new Twine\Str('john pinkerton');
+        $string2 = new Twine\Str('JoHN PiNKeRToN');
+        $string3 = new Twine\Str('BoB BeLCHeR');
+
+        $this->assertTrue($string1->equals(clone $string1));
+        $this->assertFalse($string1->equals($string2));
+        $this->assertTrue($string1->equals($string2, Twine\Config\Equals::CASE_INSENSITIVE));
+        $this->assertFalse($string1->equals($string3, Twine\Config\Equals::CASE_INSENSITIVE));
+    }
+
     public function test_it_can_determine_if_it_starts_with_a_string()
     {
         $string = new Twine\Str('john pinkerton');
